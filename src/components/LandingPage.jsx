7d5219b81e4c377e4749239e73b3a9b5516dffe3
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-06-13 12:38:42 -0400

    component setup

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+
+function LandingPage(){
+    return(
+        <div>
+            <Navbar/>
+            <Sidebar/>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aspernatur quo recusandae aliquam rem, expedita saepe nesciunt necessitatibus modi consectetur?
+        </div>
+    )
+}
+
+export default LandingPage;
