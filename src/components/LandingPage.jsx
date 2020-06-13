74a27a954190671628286f748b397f289e1adad8
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-06-13 12:47:33 -0400

    footer component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
+import Footer from "./Footer"
 
 function LandingPage() {
     return (
@@ -8,6 +9,7 @@ function LandingPage() {
             <Navbar />
             <Sidebar />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aspernatur quo recusandae aliquam rem, expedita saepe nesciunt necessitatibus modi consectetur?
+            <Footer/>
         </div>
     )
 }
