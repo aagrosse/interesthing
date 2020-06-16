6c77f0cbd2c8fb12cc3959adb6c32475f8a3fed1
aagrosse <aagrock81@yahoo.com>
2020-06-16 02:13:20 -0400

    added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./pages/LandingPage/LandingPage"
+import Admin from "./pages/Dashboard/Dashboard"
 
 function App() {
     return (
       <Router>
         {/* <StoreProvider> */}
           <Route exact path='/' component={LandingPage} />
+          <Route exact path='/dashboard' component={Admin} />
         {/* </StoreProvider> */}
       </Router>
   );
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
+import Navbar from "../../components/Navbar"
+import Sidebar from "../../components/Sidebar"
+import Footer from "../../components/Footer"
 
 function LandingPage() {
     return (
